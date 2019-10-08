379ef5fe83635253ae6592b9813c69d9f2cbb590
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2019-10-08 14:12:41 -0700

    Add a simple test1 variable.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,6 +3,7 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
+	private int test1 = 1;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
