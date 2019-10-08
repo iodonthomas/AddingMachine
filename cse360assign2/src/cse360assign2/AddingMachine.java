ff88d06ea6deec5da043202a184c4938c3b34ce0
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2019-10-08 14:47:03 -0700

    everything except comments

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -3,26 +3,29 @@ package cse360assign2;
 public class AddingMachine {
 
 	private int total;
-	private int test1 = 1;
+	private String history;
 	
 	public AddingMachine () {
 		total = 0;  // not needed - included for clarity
+		history = "0";
 	}
 	
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	public void add (int value) {
-		
+		total += value;
+		history = history+" + "+value;
 	}
 	
 	public void subtract (int value) {
-		
+		total -= value;
+		history = history+" - "+value;
 	}
 	
 	public String toString () {
-		return "";
+		return history;
 	}
 	
 	public void clear() {
