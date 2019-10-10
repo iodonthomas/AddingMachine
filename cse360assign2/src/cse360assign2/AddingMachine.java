7c6d00a6308e1331d0239c504185b998f37b8448
learnskill11 <55130492+learnskill11@users.noreply.github.com>
2019-10-09 23:46:51 -0700

    Added External Documentation.

--- cse360assign2/src/cse360assign2/AddingMachine.java
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -8,33 +8,77 @@
 
 package cse360assign2;
 
+/**
+ * 
+ * AddingMachine contains functions to add, subtract, 
+ * return history of operations, return answer, and clear operations.
+ *
+ */
+
 public class AddingMachine {
 
 	private int total;
 	private String history;
+	
+	/**
+	 * 
+	 * Constructor initializes history and answer variables.
+	 * 
+	 */
 	
 	public AddingMachine () {
 		total = 0;  
 		history = "0";
 	}
 	
+	/**
+	 * 
+	 * return the answer variable.
+	 * 
+	 */
+	
 	public int getTotal () {
 		return total;
 	}
+	
+	/**
+	 * 
+	 * Add input value to the total and to history.
+	 * 
+	 */
 	
 	public void add (int value) {
 		total += value;
 		history = history+" + "+value;
 	}
 	
+	/**
+	 * 
+	 * Subtract input value to the total and add to history.
+	 * 
+	 */
+	
 	public void subtract (int value) {
 		total -= value;
 		history = history+" - "+value;
 	}
 	
+	/**
+	 * 
+	 * Return a string variable with history of all operations, not cleared.
+	 * 
+	 */
+	
 	public String toString () {
 		return history;
 	}
+	
+	/**
+	 * 
+	 * Sets variables back to constructor stage.
+	 * History of operations and answer return to "0".
+	 * 
+	 */
 	
 	public void clear() {
 		total = 0;
